de009342043a0462583dd6ff83061616c6ac11fd
Max Tuerpe <maxtuerpe4@gmail.com>
2018-09-12 16:41:20 -0600

    Commit 4 - Fizz Buzz answered

--- app.js
+++ app.js
@@ -9,13 +9,27 @@
 //     }
 // }
 
-for(let i = 0; i < 20; i ++){
-    const say = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
-    console.log("Love me, pet me! HSSSSSS!");
-    let randomSay = say[Math.floor(Math.random() * say.length)];
-    if (i%2 === 0){
-        console.log(randomSay);
+// for(let i = 0; i < 20; i ++){
+//     const say = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+//     let randomSay = say[Math.floor(Math.random() * say.length)];
+//     console.log("Love me, pet me! HSSSSSS!");
+//     if (i%2 === 0){
+//         console.log(randomSay);
+//     }
+// }
+
+for (let i = 1; i < 101; i++){
+    if(i%3=== 0){
+        console.log("fizz");
     }
+    if(i%5=== 0){
+        console.log("buzz");
+    }
+    if(i%3=== 0 && i%5=== 0){
+        console.log("fizzbuzz");
+    }
+    else
+    console.log(i);
 }
     
 
